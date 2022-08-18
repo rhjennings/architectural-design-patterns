a1c36ba6327b4400b1202f603670f32ff675a2c5
Ron Jennings <rhjennings@gmail.com>
2022-08-18 16:39:52 +0000

    initial checkin

---
+++ src/test/java/com/jitterted/ebp/blackjack/HexArchTest.java
@@ -0,0 +1,26 @@
+package com.jitterted.ebp.blackjack;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.lang.ArchRule;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
+public class HexArchTest {
+    @DisplayName("domain should not depend on adpater package")
+    @Test
+    void domainMustNotDependOnAdapter() {
+        JavaClasses importedClasses = 
+            new ClassFileImporter().importPackages("com.jitterted.ebp.blackjack");
+        
+        ArchRule myRule = noClasses().that().resideInAPackage("..domain..")
+            .should().dependOnClassesThat()
+            .resideInAPackage("..adapter..");
+
+        myRule.check(importedClasses);
+    }
+
+}
